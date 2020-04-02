c03475a2574fb204a558fe2171a768aa37f13fa5
Uliana Bobrova <uliana.bobrova@gmail.com>
2020-04-02 12:08:56 +0300

    1 commit Lesson05

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
+"use strict"
+
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
 let money, income, addExpenses, deposit, mission, period;
 
-money = +prompt('Ваш месячный доход?');
+let start = function() {
+    do { 
+        money = +prompt('Ваш месячный доход?');
+        
+    }
+    while (!isNumber(money))
+
+    console.log(money);
+
+};
+ start();
+
 income = 'Фриланс';
 addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'бензин, налоги, одежда');
 deposit = confirm('Есть ли у вас депозит в банке?');
 mission = 17000;
 period = 6;
 
-let expenses1 = prompt('Введите обязательную статью расходов?', 'бензин');
-let amount1 = +prompt('Во сколько это обойдется?');
-let expenses2 = prompt('Введите обязательную статью расходов?', 'аренда машины');
-let amount2 = +prompt('Во сколько это обойдется?');
+
+// let expenses1 = prompt('Введите обязательную статью расходов?', 'бензин');
+// //let amount1 = +prompt('Во сколько это обойдется?');
+// let expenses2 = prompt('Введите обязательную статью расходов?', 'аренда машины');
+// let amount2 = +prompt('Во сколько это обойдется?');
+let expenses = [];
+//let amount2, amount1;
 
 let showTypeOf = function(data) {
     console.log(data, typeof(data));
@@ -19,30 +39,67 @@ showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
 
-function getExpensesMonth(a1, a2) {    //расходы за месяц
-    const sum = a1 + a2;
-    return sum;
-    } 
-    console.log('Сумма обязательных расходов за месяц: ', + getExpensesMonth(amount1, amount2) + ' рублей');
 
-    console.log('Возможные расходы: ', addExpenses.toLowerCase().split(', '));
+let getExpensesMonth = function() { 
+    let sum = 0;
+    let partSum = 0;
 
-function getAccumulatedMonth(a1, a2, money) {     //возвращает накопления за месяц (=budgetMonth)
-        const res = money - (a1 + a2);
-        return res;
+    for (let i = 0; i < 2; i++)    // ввод двух сотавляющих итоговой суммы (два прохода)
+    {
+
+    expenses[i] = prompt('Введите обязательную статью расходов?', 'бензин'); 
+
+    do {
+        partSum = +prompt('Во сколько это обойдется?'); 
     }
-    let accumulatedMonth = getAccumulatedMonth(amount1, amount2, money); //присваиваем переменной результат вызова функции 
-        //console.log('Вы можете отложить в накопление: ', + accumulatedMonth + ' рублей');
+     while (!isNumber(partSum)); 
+
+     sum = sum + partSum;
+
+    }  // конец ввода составляющих итоговой суммы
+    
+    console.log(expenses);
+    return sum;
+     
+}   // end of function расходы за месяц
+
+let expensesAmount = getExpensesMonth();
+
+
+console.log('Сумма обязательных расходов за месяц: ', + expensesAmount + ' рублей');
+
+
+//console.log('Возможные расходы: ', addExpenses.toLowerCase().split(', '));
+
+let getAccumulatedMonth =  function() {     //возвращает накопления за месяц (=budgetMonth)
+    return money - expensesAmount;
+    }
+    let accumulatedMonth = getAccumulatedMonth(); //присваиваем переменной результат вызова функции 
+        console.log('Вы можете отложить в накопление: ', + accumulatedMonth + ' рублей');
 
 let budgetDay;    //бюджет на день
     budgetDay = Math.floor(accumulatedMonth/30);
     console.log('Бюджет на день: ', + budgetDay + ' рублей');
 
-function getTargetMonth(mis, month) {    //считаем период достижения цели
-    let goalMonth = Math.ceil(mis / month);
+
+
+
+let getTargetMonth = function() {    //считаем период достижения цели
+    
+    let goalMonth = Math.ceil(mission / accumulatedMonth);
     return goalMonth;
+}   // конец - считаем период достижения цели
+    
+
+
+if (getTargetMonth() > 0) {
+        console.log('Цель будет достигнута за: ', getTargetMonth(), 'месяца');
     }
-    console.log('Цель будет достигнута за: ', getTargetMonth(mission, accumulatedMonth), 'месяца');
+  else {
+      console.log('Цель будет не достигнута');
+  }
+    
+  
 
 let getStatusIncome = function() {
     if (budgetDay >= 1200) {
@@ -56,6 +113,18 @@ let getStatusIncome = function() {
 }
     console.log(getStatusIncome());
 
+
+
+
+
+
+// let send = prompt('Введите ваше сообщение: ');
+
+// let stringCheck = function(callback) {
+// if (typeof send === 'string');
+// callback();
+// console.log('Допустимо вводить только буквы');
+// }
 
 
 //console.log(addExpenses.length);
@@ -98,11 +167,14 @@ let getStatusIncome = function() {
 
 // //условия через многомерный массив
 // let lang2 = prompt('Введите язык ru или en, чтобы увидеть дни недели: ', 'en');
-// let arr = [
-//     ['monday, tuesday, wednesday, thursday, friday, saturday, sunday'],
-//     ['понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'],
-// ];
+// let arr = new Map([
+//     ['en', 
+//     ['monday, tuesday, wednesday, thursday, friday, saturday, sunday']],
+//     ['ru',
+//     ['понедельник, вторник, среда, четверг, пятница, суббота, воскресенье']],
+// ]);
 
+// arr.get('en')
 // let chooseLang = lang2 === 'en' ? arr[0][0] : arr[1][0];
 // console.log('Результат: ', chooseLang);
 
